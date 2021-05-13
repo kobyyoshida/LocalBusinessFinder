8ecb441ded4345469a02b352decf86a5ec3095ef
Koby <koyoshida@chapman.edu>
2021-05-13 14:05:53 -0700

    creates csv file with results, but csv needs to be reread

--- finder.py
+++ finder.py
@@ -1,39 +1,42 @@
 import requests
 import json
 import csv
- 
+
 api_key = 'fiyrU_gFRWeYqSbT66zWEGmKoY7_MAO_t4zkUPMaiHbD6_i4n4cN7_0h16ihNg-07yJUR7_toMqhmJib_KeNQCvMtgLKpcdAmHtbu0NclwxDgU06eAxdXsTtuIadYHYx'
 headers = {'Authorization': 'Bearer %s' % api_key}
  
 url = 'https://api.yelp.com/v3/businesses/search'
 params = {'term':'veterinarian','location':'Orange County'}
- 
+
 req = requests.get(url, params=params, headers=headers)
  
 parsed = json.loads(req.text)
  
 businesses = parsed["businesses"]
+
+with open('businesses.csv', 'w', newline='') as csvfile:
+    w = csv.writer(csvfile, delimiter=' ',
+                            quotechar=',', quoting=csv.QUOTE_MINIMAL)
+
+    w.writerow(['Name', 'Rating', 'Address', 'Phone'])
  
-for business in businesses:
-    print("Name:", business["name"])
-    print("Rating:", business["rating"])
-    print("Address:", " ".join(business["location"]["display_address"]))
-    print("Phone:", business["phone"])
-    print("\n")
- 
-    id = business["id"]
- 
-    url="https://api.yelp.com/v3/businesses/" + id + "/reviews"
- 
-    req = requests.get(url, headers=headers)
- 
-    parsed = json.loads(req.text)
- 
-    reviews = parsed["reviews"]
- 
-    print("--- Reviews ---")
- 
-    #for review in reviews:
-    #    print("User:", review["user"]["name"], "Rating:", review["rating"], "Review:", review["text"], "\n")
+    for business in businesses:
+        print("Name:", business["name"])
+        print("Rating:", business["rating"])
+        print("Address:", " ".join(business["location"]["display_address"]))
+        print("Phone:", business["phone"])
+        print("\n")
+        
+        w.writerow([ business["name"],business["rating"]," ".join(business["location"]["display_address"]),business["phone"]])
+    
+        id = business["id"]
+    
+        url="https://api.yelp.com/v3/businesses/" + id + "/reviews"
+    
+        req = requests.get(url, headers=headers)
+    
+        parsed = json.loads(req.text)
+    
+        reviews = parsed["reviews"]
  
  
